e12097bda4f9cfa91a3985fee910159094a4b00c
SprengerV <b.francis@sprengerv.com>
2021-05-17 19:23:53 -0400

    added carousel with item components

--- src/views/projects.js
+++ src/views/projects.js
@@ -1,9 +1,63 @@
 import React from 'react';
 import Footer from '../components/footer';
+import CarouselItem from '../components/carouselItem';
 
-const Projects = () => {
+const ps = [
+  {
+    src: '',
+    label: '',
+    caption: ''
+  },
+  {
+    src: '',
+    label: '',
+    caption: ''
+  },
+  {
+    src: '',
+    label: '',
+    caption: ''
+  },
+  {
+    src: '',
+    label: '',
+    caption: ''
+  },
+  {
+    src: '',
+    label: '',
+    caption: ''
+  },
+  {
+    src: '',
+    label: '',
+    caption: ''
+  }
+];
+
+const Projects = () => { 
   return (
     <main>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          { ps.map((p, i) => (
+            <CarouselItem key={ i } src={ p.src } label={ p.label } caption={ p.caption }/>
+          )) }
+        </div>      
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
       <Footer/>
     </main>
   );
