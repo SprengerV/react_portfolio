21e27a729b958006670c829e8b7ac2fde506e999
SprengerV <b.francis@sprengerv.com>
2021-05-17 10:46:58 -0400

    styling changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,59 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
-
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-secondary">
+    <nav className="navbar navbar-expand-md navbar-dark bg-secondary" id="navbar">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand ms-md-3" to="/">
           Brendan Francis
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navCollapse">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">            <ul className="navbar-nav ms-auto me-0 mb-2">
-          <li className="nav-item">
-            <Link 
-              className={
-                window.location.pathname === "/"
-                  ? "nav-link active"
-                  : "nav-link"
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav ms-auto me-3 mb-2">  
+            <li className="nav-item">
+              <Link 
+                className={
+                  window.location.pathname === "/"
+                    ? "nav-link active"
+                    : "nav-link"
                 } 
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link 
-              className={
-                window.location.pathname === "/projects"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-              to="/projects"
-            >
-              Projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className={
-                window.location.pathname === "/contact"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-              to="/contact"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link 
+                className={
+                  window.location.pathname === "/projects"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+                to="/projects"
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={
+                  window.location.pathname === "/contact"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+                to="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
-}
+};
 
 export default Navbar;
