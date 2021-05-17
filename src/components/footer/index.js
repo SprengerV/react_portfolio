99d1965ab41d7747bbbc0161880267d16acfa473
SprengerV <b.francis@sprengerv.com>
2021-05-17 12:39:16 -0400

    footer added

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className="fixed-bottom text-white bg-secondary container-fluid text-center d-flex flex-column justify-content-center" id="footer">
+      <div>Sprenger V LLC, 2020</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/views/projects.js
+++ src/views/projects.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Footer from '../components/footer';
 
-function Projects() {
+const Projects = () => {
   return (
-    <div>projects</div>
+    <main>
+      <Footer/>
+    </main>
   );
 };
 
