5e5b1f5fd9e939cea63af137cc0edc75f0da1b96
SprengerV <B.Francis@SprengerV.com>
2021-05-30 13:54:10 -0400

    style, added contact cards

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row, Col, Card, Link, Image, ResponsiveEmbed } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Card, CardGroup, Image } from 'react-bootstrap';
 import github120 from '../assets/img/github120.png';
 import linkedin from '../assets/img/linkedin.png';
 import documentlg from '../assets/img/documentlg.png';
@@ -7,22 +8,22 @@ import resume from '../assets/doc/Resume.pdf';
 
 const contacts = [
   {
+    name: 'Resume',
+    userName: 'Click to download ',
+    url: resume,
+    icon: documentlg
+  },
+  {
     name: 'GitHub',
-    userName: 'SprengerV',
+    userName: 'SprengerV ',
     url: 'https://github.com/SprengerV',
     icon: github120
   },
   {
     name: 'LinkedIn',
-    userName: 'Brendan Francis',
+    userName: 'Brendan Francis ',
     url: 'https://www.linkedin.com/in/brendan-francis-520b921ba/',
     icon: linkedin
-  },
-  {
-    name: 'Resume',
-    userName: 'Click to download ',
-    url: resume,
-    icon: documentlg
   }
 ]
 
@@ -31,17 +32,18 @@ function Contact() {
     <Container fluid={ true }>
       <Row className="d-flex justify-content-center">
         { contacts.map((contact, index) => <Col xs="3">
-          <Card border="p-dark" bg="light">
+          <Card border="p-dark" className="">
             <Card.Header className="bg-primary text-white">
               { contact.name }
             </Card.Header>
-            <Card body className="container-fluid d-flex flex-column justify-content-around">
-              <Row>
-              <Col xs="10">
-                <Image src={ contact.icon }/>
-              </Col>
-              </Row>
-            </Card>
+            <Card.Body>
+              <Container fluid={true} className="contact d-flex flex-column justify-content-around">
+                <Image src={ contact.icon } className="social col-8 ms-auto me-auto"/>
+                <a href={ contact.url } target="_blank" rel="noreferrer" className="linkyBoi iii ms-auto me-auto mt-4 text-center">
+                  { contact.userName }   &#10064;
+                </a>
+              </Container>
+            </Card.Body>
           </Card>
         </Col>) }
       </Row>
