bc4250cf9418c2cdbf0a862f6ce23ac355dded83
SprengerV <B.Francis@SprengerV.com>
2021-06-01 20:03:58 -0400

    debug

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Container fluid={ true }>
         <Navbar/>
         <Route
-          exact path="/"
+          path="/"
           component={ Home }
         />
         <Route
